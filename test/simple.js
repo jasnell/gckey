e6a5b8ad949eccde08afd89ad0fbb2627aceb02b
James M Snell <jasnell@gmail.com>
2018-03-18 06:45:41 -0700

    Slightly improve test to make sure counter operation

--- test/simple.js
+++ test/simple.js
@@ -2,16 +2,20 @@ const tape = require('tape')
 const assert = require('assert')
 const { GCKey, getGCCount } = require('../')
 
+const count = 10
+
 tape('simple', function (t) {
   const wm = new WeakMap()
   {
-    let n = { m: new GCKey() }  // m will garbage collect when n is
-    wm.set(n, 1)
+    for (let a = 0; a < count; a++) {
+      let n = { m: new GCKey() }  // m will garbage collect when n is
+      wm.set(n, 1)
+    }
   }
   gc()
   setImmediate(() => {
     gc()
-    assert(getGCCount(), 1)
+    assert(getGCCount(), count)
     t.end()
   });
 })
